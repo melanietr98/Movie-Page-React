c30d725230fbd219e734d58c54144f847dd241f1
Melanie R <102996216+melanietr98@users.noreply.github.com>
2023-02-15 12:49:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=db2cc2d1";
+const API_URL = "http://www.omdbapi.com?apikey={apikeyhere}";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
